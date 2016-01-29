4df17bda7374d4e455808018d6b18f2d084bcb51
Teddy Reed <teddy@prosauce.org>
2016-01-28 21:30:33 -0800

    Update for 'auto' branch

--- test_samples.py
+++ test_samples.py
@@ -2,10 +2,12 @@
 
 import os
 
-from uefi_firmware import uefi
-from uefi_firmware.misc import checker
+import uefi_firmware
+from uefi_firmware.utils import flatten_firmware_objects
 
-types = {
+# The types set bins various folder representing firmware types
+# to the expected 'detected' type from the parser code.
+TYPES = {
     "DellPFS": "dell_pfs",
     "UEFIFirmwareVolume": "uefi_volume",
     "FlashDescriptor": "flash",
@@ -13,47 +15,73 @@ types = {
     "EFICapsule": "efi_capsule",
 }
 
-def test_files(files, type_name):
-    for sample in files:
-        with open(sample, "rb") as fh:
-            sample_data = fh.read()
-        matched_type = None
-        matched_parser = None
-        for tester in checker.TESTERS:
-            if tester().match(sample_data[:100]):
-                matched_type = tester().name
-                matched_parser = tester().parser
-                break
-        if matched_type is None:
-            print ("Cannot parse (%s): No matched type." % sample)
-            return 1
-        if matched_type != type_name:
-            print ("Problem parsing (%s): mismatched type " +
-                "expected %s, got %s") % (sample, matched_type, type_name)
-            return 1
-        firmware = matched_parser(sample_data)
-        try:
-            status = firmware.process()
-        except Exception as e:
-            # Wrap 'process' in exception handling for a pretty print.
-            print ("Exception parsing (%s): (%s)." % (sample, str(e)))
-            return 1
+# The files set binds explicit file names to the number of ojects
+# detected. Modifications to the codebase should have expected changes
+# in the addition/removal of objects.
+FILES = {
+    "dell_pfs/M4600A16.pfs": 2723,
+    "flash/Latitude-E6410-A10.flash": 1186,
+    "intel_me/dell_1176upd.me": 2,
+    "uefi_volume/MINNOW1.fd": 664,
+    "efi_capsule/MBP111_0138_B16_LOCKED.scap": 3043,
+}
 
-        # Check that 'process' does not encounter invalid formats/errors.
-        if not status:
-            print ("Error parsing (%s): failure in process." % (sample))
-            return 1
+class Status(object):
+    def __init__(self, code, firmware=None):
+        self.code = code
+        self.firmware = firmware
 
-        # Attempt to iterate each of the nested/parsed objects.
-        try:
-            for object in firmware.iterate_objects():
-                pass
-        except Exception as e:
-            print ("Exception iterating (%s): (%s)." % (sample, str(e)))
-            return 1
 
-        print ("Parsing (%s): success" % (sample))
-    return 0
+def test_file(sample, type_name):
+    with open(sample, "rb") as fh:
+        sample_data = fh.read()
+    parser = uefi_firmware.AutoParser(sample_data)
+    if parser.type() is None:
+        print ("Cannot parse (%s): No matched type." % sample)
+        return Status(1)
+    if parser.type() != type_name:
+        print ("Problem parsing (%s): mismatched type " +
+            "expected %s, got %s") % (sample, parser.type(), type_name)
+        return Status(1)
+    try:
+        firmware = parser.parse()
+    except Exception as e:
+        # Wrap 'process' in exception handling for a pretty print.
+        print ("Exception parsing (%s): (%s)." % (sample, str(e)))
+        return Status(1)
+
+    # Check that 'process' does not encounter invalid formats/errors.
+    if firmware is None:
+        print ("Error parsing (%s): failure in process." % (sample))
+        return Status(1)
+
+    # Attempt to iterate each of the nested/parsed objects.
+    try:
+        for _object in firmware.iterate_objects():
+            pass
+    except Exception as e:
+        print ("Exception iterating (%s): (%s)." % (sample, str(e)))
+        return Status(1)
+
+    print ("Parsing (%s): success" % (sample))
+    return Status(0, firmware)
+
+
+def test_items(sample, firmware):
+    if sample not in FILES:
+        return Status(0)
+    objects = firmware.iterate_objects()
+    all_objects = flatten_firmware_objects(objects)
+    num_objects = len(all_objects)
+    if num_objects != FILES[sample]:
+        print ("Inconsistency parsing (%s): expected %d objects, found: %d" % (
+            sample, FILES[sample], num_objects))
+        print ("This 'may' be expected if this change improves the object " +
+            "discovery/parsing logic.")
+        return Status(1)
+    print ("Listing (%s): item count: %d" % (sample, num_objects))
+    return Status(0)
+
 
 def get_files(dir):
     files = []
@@ -62,10 +90,15 @@ def get_files(dir):
             files.append(os.path.join(base, filename))
     return files      
 
+
 if __name__ == "__main__":
-    for type_name, samples_dir in types.iteritems():
+    for type_name, samples_dir in TYPES.iteritems():
         sample_files = get_files(samples_dir)
-        status = test_files(sample_files, type_name)
-        if status > 0:
-            exit(status)
+        for sample in sample_files:
+            status = test_file(sample, type_name)
+            if status.code > 0:
+                exit(status.code)
+            status = test_items(sample, status.firmware)
+            if status.code > 0:
+                exit(status.code)
     exit(0)
